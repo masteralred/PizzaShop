6d93192407629ce4520d58299c70a553c6b71e70
Alexander Zhadnov <masteralred@gmail.com>
2016-01-21 11:15:07 +0300

    Display products on main page, step 1

--- public/app.js
+++ public/app.js
@@ -1,7 +1,10 @@
 function something()
 	{
-		var x = window.localStorage.getItem('ccc');
-		x = x * 1 + 1;
-		window.localStorage.setItem('ccc', x);
-		alert(x);
+		var x = window.localStorage.getItem('ccc'); // x = hh['ccc']
+
+		x = x * 1 + 1; // x = x + 1 // x * 1 is because in JS first row return as a string. Here we remake it to integer again
+
+		window.localStorage.setItem('ccc', x); // hh['ccc'] = x
+
+		alert(x); // Execute a table in browser
 	}
