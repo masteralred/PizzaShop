b32928b1fad6f81ac0d4ffc4e03d3dc1a33b6c0d
Alexander Zhadnov <masteralred@gmail.com>
2016-01-23 13:46:48 +0300

    New function cart_items by Zhuravl

--- public/app.js
+++ public/app.js
@@ -9,15 +9,16 @@ function something()
 		alert(x); // Execute a table in browser
 	}
 
-function add_to_cart(id)
+function add_to_cart(id)										// Функция добавления продукта в корзину (точнее создания пары в хеше localStorage)
 	{
 		var key = 'product_' + id;
 		var x = window.localStorage.getItem(key);
 		x = x * 1 + 1;
 		window.localStorage.setItem(key, x);
+		alert('Items in your cart: ' + cart_items2());
 	}
 
-function iterator()
+function iterator()												// Мой вариант вывода всех пар из localStorage
 	{
 		var list = "List of all pairs in local storage hash:";
 		for (var i = 0, len = localStorage.length; i < len; i++) {
@@ -28,7 +29,7 @@ function iterator()
 		alert(list);
 	}
 
-function cart_items()
+function cart_items()											// Мой вариант написания функции количества товаров
 	{
 		var total = 0;
 		for(var i=0, len=localStorage.length; i<len; i++) {
@@ -36,5 +37,20 @@ function cart_items()
     	var value = localStorage[key] * 1;
     	total = total + value;
 		};
-		alert('Total items in cart: ' + total)
+		alert('Total items in cart: ' + total);
+	}
+
+function cart_items2()
+	{
+		var cnt = 0;											// Задание переменной
+		for(var i = 0; i < window.localStorage.length; i++)		// Итерация (итератор начнёт с нулевого индекса; закончит на 1 меньше чем длинна массива; 
+		{														// шаг - прибавление)
+			var key = window.localStorage.key(i);				// Получаем ключ
+			var value = window.localStorage.getItem(key);		// Получаем значение по ключу
+			if (key.indexOf('product_') == 0) 					// Условие: ключ начинается с 'product_' (тут нулевая позиция)
+			{
+				cnt = cnt + value * 1;							// Сложение всех значений в результате итерации в одну переменную
+			};
+		};
+		return cnt;												// Возврат переменной
 	}
